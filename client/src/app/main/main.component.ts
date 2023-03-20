e145a33937d2e10e3fd0bb2ecffe6151fbc95d54
mostafa351 <eng.mostafa.salah.84@gmail.com>
2023-03-20 23:39:52 +0200

    hero and carousel

--- client/src/app/main/main.component.ts
+++ client/src/app/main/main.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
-
+  images = ["../../assets/Banner1.png", "../../assets/Banner2.png", "../../assets/Banner1.png"];
 }
