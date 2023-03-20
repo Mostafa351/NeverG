f845e91caff52680b39cc3c7b6ee4bb565678605
mostafa351 <eng.mostafa.salah.84@gmail.com>
2023-03-20 18:07:28 +0200

    adding header and navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
